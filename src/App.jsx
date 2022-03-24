db62497d54e6dd291caae39f8faf7e7886467950
Ru Yi Shen <ruyishen2012@gmail.com>
2022-03-24 15:19:30 -0300

    feat : pages and components files creation - status ok

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Session from "./pages/Session";
+import Success from "./pages/Success";
+
+import Header from "../components/Header";
+
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/movie/:id" element={<Movie />} />
+                <Route path="/session/:id" element={<Session />} />
+                <Route path="/success" element={<Success />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
