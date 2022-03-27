2567e03603cea566c25169002339e3c5efc05468
Ru Yi Shen <ruyishen2012@gmail.com>
2022-03-27 16:48:48 -0300

    feat (NotFound) : not found page implement - status ok

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,7 +1,21 @@
-import './style.css';
+import { Link } from 'react-router-dom';
+import styled from "styled-components";
 
 export default function NotFound() {
     return (
-        <></>
+        <Div>
+            <h1>404 - Not Found!</h1>
+            <Link to="/">Go Home</Link>
+        </Div>
     )
 }
+
+const Div = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+`;
