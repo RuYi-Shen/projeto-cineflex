b104f26dd97673a6a88ff05c26cefc40c772947e
Ru Yi Shen <ruyishen2012@gmail.com>
2022-03-27 15:51:21 -0300

    feat (Header) : bonus back button implement - status ok

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,20 @@
 import styled from "styled-components";
+import { useNavigate, useLocation } from 'react-router-dom';
+import backIcon from "../../assets/back-icon.png";
 
 export default function Header() {
+    const navigate = useNavigate();
+    const { pathname } = useLocation();
+
     return (
         <Title>
+            { pathname !== "/" ?
+                <button onClick={()=>navigate(-1)}>
+                    <img src={backIcon} alt="back-icon" />
+                </button> 
+                :
+                <></>
+            }
             <h1>CINEFLEX</h1>
         </Title>
     );
@@ -23,6 +35,20 @@ const Title = styled.header`
     background: #C3CFD9;
 
     z-index: 1;
+
+    button {
+        position: absolute;
+        top: calc(67px/2 - 20px);
+        left: 10px;
+        background: none;
+        border: none;
+        
+        img {
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+        }
+    }
 
     h1 {
         font-family: 'Roboto';
